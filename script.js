d2f3e403e21f7a32ede449913b97480d3374d805
saning1216 <saning.zhang@mail.utoronto.ca>
2024-01-31 14:28:42 -0500

    new changes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const bsn= document.getElementById("my_button");
-bsn.addEventListener("click", function(){
-    console.log(7);
-})
+
+var yesVoteCount = 0;
+var noVoteCount = 0;
+
+function vote(opinion) {
+    if (opinion === 'Yes') {
+        yesVoteCount += 1;
+        document.getElementById('yesVotes').innerText = yesVoteCount;
+    } else if (opinion === 'No') {
+        noVoteCount += 1;
+        document.getElementById('noVotes').innerText = noVoteCount;
+    }
+}
